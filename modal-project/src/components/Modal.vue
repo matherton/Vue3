9c86c1421145e6000bd6205070dcb701994d1591
Mark Atherton <mark@insightinternet.co.uk>
2023-10-28 11:24:08 +0100

    added scoped styles to Modal.vue so that styles only apply to the component

--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="backdrop" @click="$emit('close')">
-    <div class="modal" @click.stop>
+    <div class="modal">
+      <h1>Modal Title</h1>
       <p>modal content</p>
     </div>
   </div>
@@ -20,5 +21,10 @@
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
 }
 </style>
